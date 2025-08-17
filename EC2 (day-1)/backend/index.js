245010d140f4c9fe998c29959e51283a55f2ecbf
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2025-08-17 14:39:07 +0530

    Update index.js

--- EC2 (day-1)/backend/index.js
+++ EC2 (day-1)/backend/index.js
@@ -15,5 +15,5 @@ app.use("/notes", notesRouter);
 // Start server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Servers running on http://localhost:${PORT}`);
 });
